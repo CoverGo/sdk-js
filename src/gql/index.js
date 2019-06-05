a80ca89e70905b0ef183068f68c7f257abe6b94f
Alex Montgomery <alexpm@gmail.com>
2019-06-05 12:02:14 +0800

    fix(dependencies): still missing dependencies

--- src/gql/index.js
+++ src/gql/index.js
@@ -1,4 +1,4 @@
-import {fetch} from 'cross-fetch'
+import { fetch } from 'cross-fetch'
 // fix module dependencies
 
 const coverFetch = async ({query, variables = {}, token = '', locale = "en", __debug = true} ={}) => {

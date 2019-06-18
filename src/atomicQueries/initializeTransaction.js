d0b7028d15c8d11a03427cc154d14a33cba8879d
Denis Tsoi <denistsoi@gmail.com>
2019-06-18 19:37:51 +0800

    feat(mutations): update processTransaction and initialiseTransaction with api fields;

--- src/atomicQueries/initializeTransaction.js
+++ src/atomicQueries/initializeTransaction.js
@@ -2,13 +2,11 @@ import { gql } from '../gql'
 
 const initializeTransaction = gql`
   mutation initializeTransaction($initializeTransactionInput: initializeTransactionInput!){
-    addFact(initializeTransactionInput: $initializeTransactionInput){
-      errors
-      value {
-        token
-        transactionid
-        url
-      }
+    errors
+    value {
+      token
+      transactionId
+      url
     }
   }
 `.withFieldErrorMapping()

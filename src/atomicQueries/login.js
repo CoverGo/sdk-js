fe3fe101642736387b8d0d7fc5ae0c42ac986607
Denis Tsoi <denistsoi@gmail.com>
2019-06-19 11:52:46 +0800

    feat(query) jsdoc for login query;

--- src/atomicQueries/login.js
+++ src/atomicQueries/login.js
@@ -1,5 +1,14 @@
 import { gql } from '../gql'
 
+/**
+ * login
+ * @param {Object} variables - gql variables
+ * @param {string} variables.tenantId
+ * @param {string} variables.clientId
+ * @param {string} variables.username
+ * @param {string} variables.password
+ *
+ */
 const login = gql`
   query login($tenantId: String!, $clientId: String!, $username: String!, $password: String!){
     token_2(tenantId: $tenantId, clientId: $clientId, username: $username, password: $password) {

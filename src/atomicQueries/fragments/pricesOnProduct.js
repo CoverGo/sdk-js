1201f0f6e813d97f4d908b90155e53ac53836819
John Valustik <valustik@gmail.com>
2019-07-11 12:51:17 +0800

    fix(fragment): enhance prices fragment

--- src/atomicQueries/fragments/pricesOnProduct.js
+++ src/atomicQueries/fragments/pricesOnProduct.js
@@ -32,7 +32,32 @@ const PricesFragment = /* GraphQL */ `
 			indicativePrices @include(if: $hasAdvisorId) {
 				type
 				amount
+				currencyCode
+				formattedOriginalPrice
 				formattedPrice
+				originalPrice
+
+				appliedDiscounts {
+					calculationLogic
+					code
+					ratio
+					flat
+					order
+					originalPrice
+					formattedOriginalPrice
+					formattedFlat
+				}
+
+				appliedTaxes {
+					code
+					name
+					ratio
+					flat
+					order
+					originalPrice
+					formattedOriginalPrice
+					formattedFlat
+				}
 			}
 		}
 	}
